6a3da91c37e2e132c516a3b02ec45a601c806c2d
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2025-07-21 21:46:27 -0300

    feat: Implement database session management and add endpoint to retrieve all todos

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
-from database import engine
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from database import engine, session_local
+from sqlalchemy.orm import Session
 import models
 
 app = FastAPI() 
 
 models.base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/")
+def read_all_todos(db: db_dependency):
+    todos = db.query(models.Todos).all()
+    return {"todos": todos}
