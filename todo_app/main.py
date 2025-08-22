939be07b74ad1b7a4394ed3d0b089d2d7d2e7a7f
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2025-08-22 08:32:11 -0300

    feat- Created healthcheck endpoint and test_main file with health check test function

--- todo_app/main.py
+++ todo_app/main.py
@@ -7,6 +7,12 @@ app = FastAPI()
 
 models.base.metadata.create_all(bind=engine)
 
+
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"status": "ok"}
+
+
 app.include_router(auth.router, tags=["auth"], prefix="/auth")
 app.include_router(todos.router, tags=["todos"], prefix="/todos")
 app.include_router(users.router, tags=["users"], prefix="/users")
---
+++ todo_app/test/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+import main
+
+client = TestClient(main.app)
+
+
+def test_healthcheck():
+    response = client.get("/healthcheck")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"status": "ok"}
+
