da0fc862324c8591405dd404ed06f3d17c15057e
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2025-09-02 21:33:07 -0300

    feat- Created fixture test_todo and modified the test_read_all_todos_authenticated method to use test_todo fixture

--- todo_app/test/test_todos.py
+++ todo_app/test/test_todos.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine
+import pytest
+from sqlalchemy import create_engine, text
 from sqlalchemy.pool import StaticPool
 from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
@@ -7,6 +8,7 @@ from ..database import base, get_db
 from ..main import app
 from ..routers.auth import get_current_user
 from ..routers.todos import get_db_and_user
+from ..models import Todos
 
 SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./testdb.db'
 
@@ -48,8 +50,37 @@ app.dependency_overrides[get_db_and_user] = override_get_db_and_user
 
 client = TestClient(app)
 
-def test_read_all_todos_authenticated():
+@pytest.fixture
+def test_todo():
+    db = TestingSessionLocal()
+    new_todo = Todos(
+        title="Test Todo",
+        description="This is a test todo", 
+        owner_id=1, 
+        priority=5,
+        completed=False
+    )
+    db.add(new_todo)
+    db.commit()
+    db.refresh(new_todo)
+    yield new_todo
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM todos"))
+        connection.commit()
+
+
+def test_read_all_todos_authenticated(test_todo):
     response = client.get('/todos/')
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"todos": []}
+    assert response.json() == {
+        "todos": [
+            {
+                "title": "Test Todo",
+                "description": "This is a test todo",
+                "id": 1,
+                "priority": 5,
+                "completed": False
+            }
+        ]
+    }
     
