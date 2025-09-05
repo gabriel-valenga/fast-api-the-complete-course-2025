dadc024413105c58b8fc1db75b40cbbd6d2d93ae
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2025-09-05 20:00:19 -0300

    feat- Created test read one todo authenticated and test read one todo authenticated todo not found

--- todo_app/test/test_todos.py
+++ todo_app/test/test_todos.py
@@ -33,11 +33,13 @@ def override_get_db():
 def override_get_current_user():
     return {"username": "testuser", "id": 1, "user_role": "admin"}
 
+
 class MockUser:
     def __init__(self, id, username, role):
         self.id = id
         self.username = username
         self.role = role
+
 
 def override_get_db_and_user():
     db = TestingSessionLocal()
@@ -54,6 +56,7 @@ client = TestClient(app)
 def test_todo():
     db = TestingSessionLocal()
     new_todo = Todos(
+        id=1,
         title="Test Todo",
         description="This is a test todo", 
         owner_id=1, 
@@ -63,6 +66,7 @@ def test_todo():
     db.add(new_todo)
     db.commit()
     db.refresh(new_todo)
+    db.close()
     yield new_todo
     with engine.connect() as connection:
         connection.execute(text("DELETE FROM todos"))
@@ -83,4 +87,22 @@ def test_read_all_todos_authenticated(test_todo):
             }
         ]
     }
+
+
+def test_read_one_todo_authenticated(test_todo):
+    response = client.get('todos/todo/1')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        'id': 1, 
+        'title': 'Test Todo', 
+        'description': 'This is a test todo', 
+        'priority': 5, 
+        'completed': False
+    }
+    
+
+def test_read_one_todo_authenticated_not_found():
+    response = client.get('/todo/999')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Not Found"}
     
