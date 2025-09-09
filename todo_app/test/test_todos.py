ff9577a5a8f26b7a93ebf2e150bade8f8a824b69
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2025-09-09 08:22:35 -0300

    feat- Created update and delete todos test methods

--- todo_app/test/test_todos.py
+++ todo_app/test/test_todos.py
@@ -118,12 +118,56 @@ def test_create_todo_authenticated(test_todo):
         json=request_data
     )
     assert response.status_code == status.HTTP_201_CREATED
-    print(response.json())
-    
     db = TestingSessionLocal()
     model = db.query(Todos).filter(Todos.id == 2).first()
     assert model.title == request_data.get('title')
     assert model.description == request_data.get('description')
     assert model.priority == request_data.get('priority')
     assert model.completed == request_data.get('completed')
+
+
+def test_update_todo_authenticated(test_todo):
+    request_data = {
+        "title": "Updated Todo", 
+        "description": "This is an updated todo",
+        "priority": 4,
+        "completed": True
+    }    
+    response = client.put(
+        '/todos/todo/1',
+        json=request_data
+    )
+    assert response.status_code == status.HTTP_200_OK
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model.title == "Updated Todo"
+
+
+def test_update_todo_authenticated_not_found():
+    request_data = {
+        "title": "Updated Todo", 
+        "description": "This is an updated todo",
+        "priority": 4,
+        "completed": True
+    }    
+    response = client.put(
+        '/todos/todo/999',
+        json=request_data
+    )
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Todo not found"}    
+
+
+def test_delete_todo_authenticated(test_todo):
+    response = client.delete('/todos/todo/1')
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
+
+
+def test_delete_todo_authenticated_not_found():
+    response = client.delete('/todos/todo/999')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Todo not found"}
     
